0c3f400d730d13fee90244a6f4494f9a30cc8775
Kyle9021 <54778108+Kyle9021@users.noreply.github.com>
2021-05-20 11:47:19 -0400

    Update defender_deploy.bash

--- defender_deploy.bash
+++ defender_deploy.bash
@@ -28,7 +28,7 @@ pcc_mgmt_port=<PORT_MAPPED_TO_PORT_8083_OF_CONSOLE>
 pcc_defender_deploy_type=<SET_THIS_TO_ONE_OF_THE_OPTIONS_ABOVE>
 
 # Don't change variables below. I set them to avoid issues where I needed to reference variables within single quotes
-pcc_api_var="{\"username\":\""${pcc_cli_user}"\", \"password\":\""${pcc_cli_user_password}"\"}"
+pcc_api_var="{\"username\":\"${pcc_cli_user}\", \"password\":\"${pcc_cli_user_password}\"}"
 pcc_api_port_var="{\"port\":"${pcc_websocket_port}"}"
 
 # Check to ensure jq is installed
@@ -39,7 +39,7 @@ then
 fi
 
 # This is the part which creates the authorization token. One might consider separating this part in a different script and exporting the variable for more robust security
-pcc_api_token=$(curl -k -H "Content-Type: application/json" -d "${pcc_api_var}" https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/authenticate | jq -r '.token')
+pcc_api_token=$(curl -k -H "Content-Type: application/json" -d "${pcc_api_var}" "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/authenticate" | jq -r '.token')
 
 
 # Checks the user input
@@ -58,43 +58,43 @@ if [ "${pcc_defender_deploy_type}" != "swarm_linux_def" ] && \
 # linux_container_def
 elif [ "${pcc_defender_deploy_type}" = "linux_container_def" ]; then
         curl -sSL -k --header "authorization: Bearer ${pcc_api_token}" \
-        -X POST https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/scripts/defender.sh -d  \
+        -X POST "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/scripts/defender.sh" -d  \
         "${pcc_api_port_var}" | sudo bash -s -- -c "${pcc_hostname}" -d "none"
         exit
 
 # linux_host_def
 elif [ "${pcc_defender_deploy_type}" = "linux_host_def" ]; then
         curl -sSL -k --header "authorization: Bearer ${pcc_api_token}" \
-        -X POST https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/scripts/defender.sh -d  \
+        -X POST "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/scripts/defender.sh" -d  \
         "${pcc_api_port_var}" | sudo bash -s -- -c "${pcc_hostname}" -d "none"   --install-host
         exit
 
 
 # k8_linux_def
 elif [ "${pcc_defender_deploy_type}" = "k8_linux_def" ]; then
-        if [[ ! -f ./twistcli || $(./twistcli --version) != *""${pcc_console_version}""* ]]; then
+        if [[ ! -f ./twistcli || $(./twistcli --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/twistcli > \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/twistcli" > \
                 twistcli; chmod +x twistcli; fi; \
                 ./twistcli defender install kubernetes \
                 --namespace twistlock --monitor-service-accounts \
                 --token "${pcc_api_token}" \
-                --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address ""${pcc_hostname}":"${pcc_websocket_port}""
+                --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"
                         rm twistcli
         exit
 
 # k8_mac_def
 elif [ "${pcc_defender_deploy_type}" = "k8_mac_def" ]; then
-        if [[ ! -f ./twistcli-darwin || $(./twistcli-darwin --version) != *""${pcc_console_version}""* ]]; then
+        if [[ ! -f ./twistcli-darwin || $(./twistcli-darwin --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/osx/twistcli > \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/osx/twistcli" > \
                 twistcli-darwin; chmod +x twistcli-darwin; fi; \
                 ./twistcli-darwin defender install kubernetes \
                 --namespace twistlock --monitor-service-accounts \
                 --token "${pcc_api_token}" \
-                --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address ""${pcc_hostname}":"${pcc_websocket_port}""
+                --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"
                         rm twistcli-darwin
         exit
 
@@ -102,12 +102,12 @@ elif [ "${pcc_defender_deploy_type}" = "k8_mac_def" ]; then
 elif [ "${pcc_defender_deploy_type}" = "openshift_linux_def" ]; then
         if [[ ! -f ./twistcli || $(./twistcli --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/twistcli > \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/twistcli" > \
                 twistcli; chmod +x twistcli; fi; \
                 ./twistcli defender install openshift \
                 --namespace twistlock --monitor-service-accounts \
-                --token "${pcc_api_token}" --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address "${pcc_hostname}":"${pcc_websocket_port}"  \
+                --token "${pcc_api_token}" --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"  \
                 --selinux-enabled
         exit
 
@@ -116,32 +116,32 @@ elif [ "${pcc_defender_deploy_type}" = "openshift_linux_def" ]; then
 elif [ "${pcc_defender_deploy_type}" = "openshift_mac_def" ]; then
         if [[ ! -f ./twistcli-darwin || $(./twistcli-darwin --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/osx/twistcli > \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/osx/twistcli" > \
                 twistcli-darwin; chmod +x twistcli-darwin; fi; \
                 ./twistcli-darwin defender install openshift \
                 --namespace twistlock --monitor-service-accounts \
-                --token "${pcc_api_token}" --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address ""${pcc_hostname}":"${pcc_websocket_port}""  --selinux-enabled
+                --token "${pcc_api_token}" --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"  --selinux-enabled
         exit
 
 # swarm_linx_def
 elif [ "${pcc_defender_deploy_type}" = "swarm_linux_def" ]; then
         if [[ ! -f ./twistcli || $(./twistcli --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/twistcli > \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/twistcli" > \
                 twistcli; chmod +x twistcli; fi; \
                 sudo ./twistcli defender install swarm --token "${pcc_api_token}" \
-                --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address ""${pcc_hostname}":"${pcc_websocket_port}""
+                --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"
         exit
 
 # swarm_mac_def
 elif [ "${pcc_defender_deploy_type}" = "swarm_mac_def" ]; then
         if [[ ! -f ./twistcli-darwin || $(./twistcli-darwin --version) != *"${pcc_console_version}"* ]]; then
                 curl --progress-bar -L -k --header "authorization: Bearer ${pcc_api_token}" \
-                https://"${pcc_hostname}":"${pcc_mgmt_port}"/api/v1/util/osx/twistcli > twistcli-darwin; chmod +x twistcli-darwin; fi; \
+                "https://${pcc_hostname}:${pcc_mgmt_port}/api/v1/util/osx/twistcli" > twistcli-darwin; chmod +x twistcli-darwin; fi; \
                 sudo ./twistcli-darwin defender install swarm --token "${pcc_api_token}" \
-                --address "https://"${pcc_hostname}":"${pcc_mgmt_port}"" \
-                --cluster-address ""${pcc_hostname}":"${pcc_websocket_port}""
+                --address "https://${pcc_hostname}:${pcc_mgmt_port}" \
+                --cluster-address "${pcc_hostname}:${pcc_websocket_port}"
         exit
 fi
